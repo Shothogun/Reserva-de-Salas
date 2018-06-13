ed0173d41292a023b9874ac07221acaef349327b
alexnsouza6 <alexnsouza6@hotmail.com>
2018-06-13 10:11:42 -0300

    Adicionando o primeiro teste para alocação de sala

--- spec/controllers/rooms_controller_spec.rb
+++ spec/controllers/rooms_controller_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe RoomsController, type: :controller do
+    describe 'Criando uma sala' do
+        it 'Deveria chamar um método do controller que aloca um objeto de sala' do
+            @test_room = Room.new
+            assert @test_room
+        end
 
+        it 'Deveria selecionar o template new para renderização' do
+        
+        end
+
+        it 'Deveria chamar um método do controller que cria um objeto da sala' do
+        
+        end
+    end
 end
