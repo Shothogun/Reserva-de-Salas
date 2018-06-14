4b93536d6b43cd2c769360f16990390b9c5aedfe
Tiago Cabral <150150296@aluno.unb.br>
2018-06-13 23:58:27 -0300

    Teste de models para as salas

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe "CREATE Room" do
+    
+    context "Caso feliz" do
+      it "Cria uma nova sala com os parametros corretos" do
+        expect { 
+          Room.new(name: "Sala Maneira", location: "ICC Norte").save! 
+        }.to change(Room, :count).by(1)
+      end
+    end
+
+    context "Casos tristes" do
+      it "Erro ao tentar salvar salas sem o nome" do
+        expect { 
+          Room.new(location: "ICC Norte").save! 
+        }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+
+      it "Erro ao tentar salvar nomes menores que 3" do
+        expect { 
+          Room.new(name: "S", location: "ICC Norte").save! 
+        }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+  end
 end
